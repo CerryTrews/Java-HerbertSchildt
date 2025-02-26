1f6097eff2742b9e1593abcf7c3de7058cbdc2a8
Cerry <denis.carreto@hotmail.com>
2025-02-26 19:10:46 -0300

    Capítulo 2 - Incremento 1

---
+++ Cap. 2/Teórico/CharArithdemo.java
@@ -0,0 +1,17 @@
+/*
+Variáveis de caracteres podem ser tratadas somo inteiros.
+*/
+public class CharArithdemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch contains " + ch);
+
+        ch++; // incrementa ch
+        System.out.println("ch is now " + ch);
+
+        ch = 90; // dá a ch o valor Z
+        System.out.println("ch is now " + ch);
+    }
+}
---
+++ Cap. 2/Teórico/Inches.java
@@ -0,0 +1,16 @@
+/*
+Calcula quantas as polegadas cúbicas há em uma milha cúbica
+*/
+
+public class Inches {
+    public static void main(String[] args) {
+        long ci;
+        long im;
+
+        im = 5280 * 12;
+
+        ci = im * im * im;
+
+        System.out.println("There are " + ci + " cubic inches in cubic mile.");
+    }
+}
